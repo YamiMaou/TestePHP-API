82341966e09e9ae62da1efcc62e0eec057540c84
Maou Yami <ephyllus2@gmail.com>
2021-10-18 07:30:05 -0300

    clean main

--- resources/js/components/Welcome.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="container mt-5">
-        <div class="col-12 text-center">
-            <h1>Yamitec</h1>
-            <a href="https://yamitec.com" target="_blank">See my Portfolio</a>
-        </div>
-    </div>
-</template>
